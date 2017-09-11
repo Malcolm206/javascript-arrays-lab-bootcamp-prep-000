d9fe3428f8b14cd4dc0c38687b4de325b2cb01d7
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2017-09-11 17:42:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift(name)
+}
